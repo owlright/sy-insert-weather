b29e092315bc4638ca2288049c111f16729a6573
owlright <jhsheng@mail.ustc.edu.cn>
2024-01-25 15:08:49 +0800

    feat: 🎸 add Dock panel

--- src/index.ts
+++ src/index.ts
@@ -98,6 +98,56 @@ export default class InsertWeatherPlugin extends Plugin {
             }
         });
 
+        this.addDock({
+            config: {
+                position: "RightTop",
+                size: { width: 200, height: 0 },
+                icon: "iconWeather",
+                title: "Custom Dock",
+                hotkey: "",
+            },
+            data: {
+                text: "This is my custom dock",
+            },
+            type: DOCK_TYPE,
+            resize() {
+                console.log(DOCK_TYPE + " resize");
+            },
+            update() {
+                console.log(DOCK_TYPE + " update");
+            },
+            init: (dock: IDockModel) => {
+                if (this.isMobile) {
+                    dock.element.innerHTML = `
+                        <div class="toolbar toolbar--border toolbar--dark">
+                            <svg class="toolbar__icon"><use xlink:href="#iconEmoji"></use></svg>
+                            <div class="toolbar__text">Custom Dock</div>
+                        </div>
+                        <div class="fn__flex-1 plugin-insert-weather__dock">${dock.data.text}</div>
+                    </div>`;
+                } else {
+                    dock.element.innerHTML = `
+                        <div class="fn__flex-1 fn__flex-column">
+                            <div class="block__icons">
+                                <div class="block__logo">
+                                <!--  <svg class="block__logoicon"><use xlink:href="#iconEmoji"></use></svg>-->
+                                    ${this.i18n.byeMenu}
+                                </div>
+                                <span class="fn__flex-1 fn__space"></span>
+                                <span data-type="min" class="block__icon b3-tooltips b3-tooltips__sw" aria-label="Min ${adaptHotkey("")}">
+                                    <svg><use xlink:href="#iconMin"></use></svg>
+                                </span>
+                            </div>
+                            <div class="fn__flex-1 plugin-insert-weather__dock">
+                                ${dock.data.text}
+                            </div>
+                        </div>`;
+                }
+            },
+            destroy() {
+                console.log("destroy dock:", DOCK_TYPE);
+            },
+        });
         // this.addCommand({
         //     langKey: "showDialog",
         //     hotkey: "⇧⌘O",
