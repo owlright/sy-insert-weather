661e54f315463abbfea5ce8369f17e020cf98bb6
owlright <jhsheng@mail.ustc.edu.cn>
2024-01-26 23:14:46 +0800

    fix: 🐛 load and save stored settings
    1. get&store all provinces' names, then trigger the
    selectProvinceElement; 2. get&store the (first) Province's cities'
    names, triger the selectCityElement.

--- src/WeatherSetting.ts
+++ src/WeatherSetting.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { WEATHER_SITE_API, STORAGE_SETTINGS, CACHED_CITYS, WeatherCachedCity, dbg, readonlyWeatherCachedCity } from "./types";
+import { WEATHER_SITE_API, WeatherCachedCity, dbg, readonlyWeatherCachedCity, assert } from "./types";
 import { Setting } from "siyuan";
 import InsertWeatherPlugin from "./index";
 export default class WeatherSettings extends Setting {
@@ -42,7 +42,9 @@ export default class WeatherSettings extends Setting {
         const handleProvinceSelect = (event: Event) => {
             const selectedOpt = (event.target as HTMLSelectElement).value;
             console.log(selectedOpt);
-            try {
+            if (this.plugin.storageCities[selectedOpt].cities.length !== 0) {
+                this.setCitiesElements(this.plugin.storageCities[selectedOpt].cities);
+            } else {
                 axios.get(WEATHER_SITE_API + "dict/province/" + selectedOpt, { responseType: "json" }).then((response) => {
                     const ret = response.data;
                     if (ret["msg"] !== "success") {
@@ -50,18 +52,11 @@ export default class WeatherSettings extends Setting {
                     }
                     // console.log("citys: " + ret["data"]);
                     const cities: string[] = ret["data"].split("|");
-                    this.selectCityElement.innerHTML = ""; // clear recent information
-                    for (let i = 0; i < cities.length; i++) {
-                        const optionElement = document.createElement("option");
-                        const s = cities[i].split(",");
-                        optionElement.value = s[0];
-                        optionElement.text = s[1];
-                        this.selectCityElement.appendChild(optionElement);
-                    }
-                    this.selectCityElement.dispatchEvent(new Event("change")); // call selectElement's change event
+                    this.plugin.storageCities[selectedOpt]['cities']= cities;
+                    this.setCitiesElements(cities);
+                }).catch((e) => {
+                    console.error(e);
                 });
-            } catch (error) {
-                console.error(error);
             }
         };
         const handleCitySelect = (event: Event) => {
@@ -84,8 +79,38 @@ export default class WeatherSettings extends Setting {
         });
         // 获取所有省份名字，保存到缓存中，城市名字先为空，等到用户选择了省份之后再获取，到时同样保存到缓存中
         if (!this.plugin.storageCities) { // 如果没有缓存，发起请求获取省份信息
+            dbg("Ask weather.cma.cn for province names");
+            axios.get(WEATHER_SITE_API + "dict/province", { responseType: "json" })
+                .then((response) => {
+                    const ret = response.data;
+                    if (ret["msg"] !== "success") {
+                        throw Error("获取省份信息失败");
+                    }
+                    const provinces: string[] = ret["data"].split("|");
+                    const provinceMap_: WeatherCachedCity = {};
+                    for (let i = 0; i < provinces.length; i++) {
+                        const province_ = provinces[i].split(",");
+                        provinceMap_[province_[0]] = { province: province_[1], cities: [] as string[] };
+                    }
+                    this.plugin.storageCities = provinceMap_;
+                    this.setProvinceElements(provinceMap_);
+                    if (this.plugin.storageSettings) {
+                        this.selectProvinceElement.value = this.plugin.storageSettings.province;
+                        this.selectProvinceElement.dispatchEvent(new Event("change"));
+                        this.selectCityElement.value = this.plugin.storageSettings.city;
+                    }
+                })
+                .catch(e => {
+                    console.error(e);
+                });
+        } else { // 缓存存在直接使用
+            this.setProvinceElements(this.plugin.storageCities);
+            if (this.plugin.storageSettings) {
+                this.selectProvinceElement.value = this.plugin.storageSettings.province;
+                this.selectProvinceElement.dispatchEvent(new Event("change"));
+                this.selectCityElement.value = this.plugin.storageSettings.city;
             }
-        });
+        }
     }
 
     private setProvinceElements = (cities: readonlyWeatherCachedCity) => {
@@ -97,8 +122,19 @@ export default class WeatherSettings extends Setting {
             this.selectProvinceElement.appendChild(optionElement);
         }
         this.selectProvinceElement.dispatchEvent(new Event("change"));
-
     };
+
+    private setCitiesElements = (cities: string[]) => {
+        this.selectCityElement.innerHTML = ""; // clear recent information
+        for (let i = 0; i < cities.length; i++) {
+            const optionElement = document.createElement("option");
+            const s = cities[i].split(",");
+            optionElement.value = s[0];
+            optionElement.text = s[1];
+            this.selectCityElement.appendChild(optionElement);
+        }
+        this.selectCityElement.dispatchEvent(new Event("change")); // call selectElement's change event
+    }
 
 }
 
